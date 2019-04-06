f6299ef0e2b1c37bd50643a8f2ab29b006f6fb8b
Sambhav Dusad <sambhavdusad24@gmail.com>
2019-04-06 16:31:38 +0530

    search logic

--- controllers/search.js
+++ controllers/search.js
@@ -4,14 +4,28 @@ var account = require('../models/account');
 exports.getSearch = function(req, res){
     console.log(req.body);
     account.find().distinct('city', function(err, city){
-        city.sort();
-        if (err){
+        if(err){
             console.log(err);
         }
-        res.render('search', {
-            user: req.user, 
-            city: city,
-            results: res
+        city.sort();
+        account.find({city: req.body.city}, function(err, acc) {
+            if(err){
+                console.log(err);
+            }
+            console.log(acc);
+
+            medicine.find({name: req.body.query}, function(err, med){
+                console.log(med);
+                if(err){
+                    console.log(err);
+                }
+
+                res.render('search', {
+                        user: req.user,
+                        city: city,
+                        medicine: med
+                });
+            });
         });
     });
 };

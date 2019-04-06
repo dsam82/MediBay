d448f204cdd94a3204e1eb499e339cef2d79d9eb
vsvipul <vsvipul555@gmail.com>
2019-04-06 22:19:36 +0530

    final commit

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -2,21 +2,16 @@ var medicine = require('../models/medicine');
 var account = require('../models/account');
 exports.getMedicine = function(req, res){
     medicine.findOne({_id: req.params.id}, async function(err, med) {
-
-        // med.sort(function(a,b){
-        //     return a.createdAt - b.createdAt;
-        // });
-        console.log(med);
-        var x = '9418165156';
-        await account.find({username: med.username}, function(err, acc){
-            console.log(acc);
+        var x="";
+        await account.findOne({username: med.username}, async function(err, acc){
+            console.log(acc.phone);
             x = acc.phone;
         });
         if(err){
             console.log(err);
         }
         console.log(x);
-        await res.render('medicine', {
+        res.render('medicine', {
             user: req.user,
             medicine: med,
             contact: x
